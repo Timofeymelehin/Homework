390e4bc98bb1d1b1f72125034673f361c1e462b9
Timofeymelehin <82331857+Timofeymelehin@users.noreply.github.com>
2021-06-06 22:26:58 +0300

    MelehinTimofeyhw4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App/App';
+import ThemeContext from './context';
+import './styles.css';
+
+function ChangeTheme() {
+  const [theme, setTheme] = useState('appLight')
+  return (
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <App />
+    </ThemeContext.Provider>
+  )
+}
 
 ReactDOM.render(
-    <App />,
+  <ChangeTheme />,
   document.getElementById('root')
 );
 
